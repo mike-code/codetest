de782f01f073817fbe0d42b2b5d487b902922616
Mike Taylor <mike@ring0.cc>
2017-03-06 02:15:31 +0100

    Better CLI library and Geotools

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 
 require 'vendor/autoload.php';
 
+
 use Carbon\Carbon;
-use Commando\Command;
-use League\Geotools;
-use Geocoder\Model\Coordinates;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use League\Geotools\Coordinate\Coordinate;
+
 
 interface Drone
 {
@@ -24,11 +30,11 @@ class Costello implements Drone
 
 class X
 {
-	public function __construct()
-	{
-		$current = Carbon::now();
-		$current = new Carbon();
-	}
+    public function __construct()
+    {
+        $current = Carbon::now();
+        $current = new Carbon();
+    }
 }
 
 class VerifierException extends \Exception
@@ -38,87 +44,114 @@ class VerifierException extends \Exception
 
 class Verifier
 {
-	private $command;
-	private $tempLatitude;
+    public static function coords($latitude, $longitude)
+    {   
+        if(!is_numeric($latitude))
+        {
+            throw new VerifierException("Latitude must be a number");
+        }
 
-	public function __construct(Command $c)
-	{
-		$this->command = $c;
-	}
+        if(!is_numeric($longitude))
+        {
+            throw new VerifierException("Longitude must be a number");
+        }
 
-	public function check($method, $argument)
-	{
-		try
-		{
-			$this->{$method}($argument);
-			return true;
-		}
-		catch(VerifierException $e)
-		{
-			echo "ERR: {$e->getMessage()}", PHP_EOL;
-			return false;
-		}
-	}
-
-	private function latitude($latitude)
-	{
-		if(!is_numeric($latitude))
-		{
-			throw new VerifierException("Latitude must be a number");
-		}
-
-		$this->tempLatitude = $latitude;
-	}
-
-	/**
-	 * Longitude always comes after latitude
-	 * 
-	 * @param type $longitude 
-	 * @return type
-	 */
-	private function longitude($longitude)
-	{	
-		if(!is_numeric($longitude))
-		{
-			throw new VerifierException("Longitude must be a number");
-		}
-
-		try
-		{
-			new Coordinates($this->tempLatitude, $longitude);
-		}
-		catch(Exception $e)
-		{
-			throw new VerifierException($e->getMessage());
-		}
-	}
+        try
+        {
+            $coordinate = new Coordinate([$latitude, $longitude]);
+            echo $coordinate->getLatitude(), PHP_EOL;
+        }
+        catch(Exception $e)
+        {
+            throw new VerifierException($e->getMessage());
+        }
+    }
 }
 
-class Main
+class AddFlight extends Command
 {
-	public $command;
+    protected $commandName = 'flight:add';
+    protected $commandDescription = "Adds a drone flight";
 
-	public function __construct()
-	{
-		$this->command = new Command();
-		$v = new Verifier($this->command);
+    protected $departureLatitudeName = "departure-latitude";
+    protected $departureLatitudeDesc = "Departure location latitude (eg. 51.8860)";
 
-		$this->command->argument()->require()->describedAs('Departure location latitude (eg. 51.8860)')->must(function($value) use ($v) { return $v->check('latitude', $value); });
-		$this->command->argument()->require()->describedAs('Departure location longitude (eg. 0.2388)')->must(function($value) use ($v) { return $v->check('longitude', $value); });
-		$this->command->argument()->require()->describedAs('Departure location timezone as UTC time offset (eg. +3 or -6');
-		//$this->command->argument()->require()->describedAs('Destination location latitude (eg. 51.8860)');
-		//$this->command->argument()->require()->describedAs('Destination location longitude (eg. 0.2388)');
-		//$this->command->argument()->require()->describedAs('Destination location timezone as UTC time offset (eg. +3 or -6');
-	}
+    protected $departureLongitudeName = "departure-longitude";
+    protected $departureLongitudeDesc = "Departure location longitude (eg. 0.2388)";
+
+    protected $departureTimezoneName = "departure-timezone";
+    protected $departureTimezoneDesc = "Departure location timezone as UTC time offset (eg. +3 or -6)";
+
+    protected $destinationLatitudeName = "destination-latitude";
+    protected $destinationLatitudeDesc = "Destination location latitude (eg. 51.8860)";
+
+    protected $destinationLongitudeName = "destination-longitude";
+    protected $destinationLongitudeDesc = "Destination location longitude (eg. 0.2388)";
+
+    protected $destinationTimezoneName = "destination-timezone";
+    protected $destinationTimezoneDesc = "Destination location timezone as UTC time offset (eg. +3 or -6)";
+
+    private $departureCoords;
+    private $destinationCoords;
+
+
+    protected function configure()
+    {
+        $this
+            ->setName($this->commandName)
+            ->setDescription($this->commandDescription)
+            ->addArgument(
+                $this->departureLatitudeName,
+                InputArgument::REQUIRED,
+                $this->departureLatitudeDesc
+            )
+            ->addArgument(
+                $this->departureLongitudeName,
+                InputArgument::REQUIRED,
+                $this->departureLongitudeDesc
+            )
+            ->addArgument(
+                $this->departureTimezoneName,
+                InputArgument::REQUIRED,
+                $this->departureTimezoneDesc
+            )
+            ->addArgument(
+                $this->destinationLatitudeName,
+                InputArgument::REQUIRED,
+                $this->destinationLatitudeDesc
+            )
+            ->addArgument(
+                $this->destinationLongitudeName,
+                InputArgument::REQUIRED,
+                $this->destinationLongitudeDesc
+            )
+            ->addArgument(
+                $this->destinationTimezoneName,
+                InputArgument::REQUIRED,
+                $this->destinationTimezoneDesc
+            )
+        ;
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $departLat = $input->getArgument($this->departureLatitudeName);
+        $departLon = $input->getArgument($this->departureLongitudeName);
+        $departTime = $input->getArgument($this->departureTimezoneName);
+        $destLat = $input->getArgument($this->destinationLatitudeName);
+        $destLon = $input->getArgument($this->destinationLongitudeName);
+        $destTime = $input->getArgument($this->destinationTimezoneName);
+
+        $this->departureCoords = Verifier::coords($departLat, $departLon);
+        //$this->destinationCoords = Verifier::coords($destLat, $destLon);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
 }
 
-new Main();
-
-// $cc->useDefaultHelp();
-
-// latitude
-// longitude
-// timezone (UTC time offset)
-// latitude
-// longitude
-// timezone (UTC time offset)
+$application = new Application('Heptapod Laundrone Flight System', '0.8');
+$application->add(new AddFlight());
+$application->run();
